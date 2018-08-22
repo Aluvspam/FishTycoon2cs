893aa1daf065d4c9d092f2ffa97770b362908c9a
Cristian232 <39384968+Cristian232@users.noreply.github.com>
2018-08-22 19:58:31 +0300

    Menu Added

--- FishTycoon2cs/FishTycoon2cs/Form1.Designer.cs
+++ FishTycoon2cs/FishTycoon2cs/Form1.Designer.cs
@@ -28,20 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.breedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.costToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autoplayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fToolStripMenuItem,
+            this.breedToolStripMenuItem,
+            this.costToolStripMenuItem,
+            this.autoplayToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1404, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fToolStripMenuItem
+            // 
+            this.fToolStripMenuItem.Name = "fToolStripMenuItem";
+            this.fToolStripMenuItem.Size = new System.Drawing.Size(40, 24);
+            this.fToolStripMenuItem.Text = "Fin";
+            // 
+            // breedToolStripMenuItem
+            // 
+            this.breedToolStripMenuItem.Name = "breedToolStripMenuItem";
+            this.breedToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.breedToolStripMenuItem.Text = "Breed";
+            // 
+            // costToolStripMenuItem
+            // 
+            this.costToolStripMenuItem.Name = "costToolStripMenuItem";
+            this.costToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.costToolStripMenuItem.Text = "Cost";
+            // 
+            // autoplayToolStripMenuItem
+            // 
+            this.autoplayToolStripMenuItem.Name = "autoplayToolStripMenuItem";
+            this.autoplayToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.autoplayToolStripMenuItem.Text = "Autoplay";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1404, 509);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem breedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem costToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autoplayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
     }
 }
 
