4ed7808d6c8b402d8da4e94b28643e066485bba8
Cristian232 <39384968+Cristian232@users.noreply.github.com>
2018-08-22 19:38:00 +0300

    added charts csv

--- FishTycoon2cs/FishTycoon2cs/Form1.cs
+++ FishTycoon2cs/FishTycoon2cs/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace FishTycoon2cs
 {
     public partial class Form1 : Form
-    {
+    {   
         public Form1()
         {
             InitializeComponent();
