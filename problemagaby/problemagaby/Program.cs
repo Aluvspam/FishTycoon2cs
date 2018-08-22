bd2df2e86f636bcafd40d68b93cad5fdbbd62675
Andrei <Andrei@ANDREI-PC>
2018-08-22 19:39:28 +0300

    added problema gabi

---
+++ problemagaby/problemagaby/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace InputValidation
+{
+    class PasswordValidator
+    {
+        static void Main(string[] args)
+        {
+            int l = 6;//CONSREAD +2
+            for (int i = 0; i < l; i++)//LINIA  
+            {
+                for (int j = 0; j < l; j++)//coloana    
+                {
+                    if (i == 0 || i == l - 1)
+                    {
+                        if (j == 0 || j == l - 1)
+                        {
+                            Console.Write('+');
+                        }
+                        else
+                        {
+                            Console.Write('-');
+                        }
+                    }
+                    else
+                    {
+                        if (j == 0 || j == l - 1)
+                        {
+                            Console.Write('|');
+                        }
+                        else
+                        {
+                            Console.Write(' ');
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
